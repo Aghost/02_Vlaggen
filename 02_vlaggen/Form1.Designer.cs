1c74042ad7349eeb9e683f7aa4a350a67e003c5e
youri <a;sldkfjfjdksla>
2020-07-30 13:05:40 +0200

    updated version, todo: resize .svg, or resize-method. add question method

---
+++ 02_vlaggen/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace _02_vlaggen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imageBox = new System.Windows.Forms.WebBrowser();
+            this.btn_prev = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.btn_guess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // imageBox
+            // 
+            this.imageBox.Location = new System.Drawing.Point(12, 12);
+            this.imageBox.MinimumSize = new System.Drawing.Size(20, 20);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(776, 316);
+            this.imageBox.TabIndex = 0;
+            this.imageBox.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // btn_prev
+            // 
+            this.btn_prev.Location = new System.Drawing.Point(248, 415);
+            this.btn_prev.Name = "btn_prev";
+            this.btn_prev.Size = new System.Drawing.Size(75, 23);
+            this.btn_prev.TabIndex = 1;
+            this.btn_prev.Text = "btn_prev";
+            this.btn_prev.UseVisualStyleBackColor = true;
+            this.btn_prev.Click += new System.EventHandler(this.btn_prev_Click);
+            // 
+            // btn_next
+            // 
+            this.btn_next.Location = new System.Drawing.Point(491, 415);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(75, 23);
+            this.btn_next.TabIndex = 2;
+            this.btn_next.Text = "btn_next";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(329, 415);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(156, 20);
+            this.textBox.TabIndex = 3;
+            // 
+            // btn_guess
+            // 
+            this.btn_guess.Location = new System.Drawing.Point(329, 386);
+            this.btn_guess.Name = "btn_guess";
+            this.btn_guess.Size = new System.Drawing.Size(156, 23);
+            this.btn_guess.TabIndex = 4;
+            this.btn_guess.Text = "btn_guess";
+            this.btn_guess.UseVisualStyleBackColor = true;
+            this.btn_guess.Click += new System.EventHandler(this.btn_guess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_guess);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.btn_prev);
+            this.Controls.Add(this.imageBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser imageBox;
+        private System.Windows.Forms.Button btn_prev;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button btn_guess;
+    }
+}
+
---
+++ 02_vlaggen/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02_vlaggen
+{
+    struct flagStruct
+    {
+        public String imgName;
+        public int imgIndex;
+        // public int error-count;
+        // public bool active;
+    }
+
+    public partial class Form1 : Form
+    {
+        string imgPath = Directory.GetParent(Directory.GetParent(Environment.CurrentDirectory).ToString()).ToString()
+                            + "\\img\\"; // `cd ../img | bash`
+        string currentImage = "";
+        uint currentIndex = 0;
+
+        // string foldersize = Directory.EnumerateFiles("./").ToString();
+        flagStruct[] flagArray = new flagStruct[255]; // flag-images.count
+
+        public Form1()
+        {
+            InitializeComponent();
+            Load_Images();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Load_Images()
+        {
+            DirectoryInfo d = new DirectoryInfo(imgPath);
+            int flagCount = 0;
+
+            foreach (var file in d.GetFiles("*.svg"))
+            {
+                flagArray[flagCount].imgName = file.Name; //.Split('.').First().Remove(0, 8).Replace('_', ' ');
+                flagArray[flagCount].imgIndex = flagCount;
+                
+                Console.WriteLine($"{flagArray[flagCount].imgName}\n{flagArray[flagCount].imgIndex}");
+                flagCount += 1;
+            }
+        }
+
+        private void btn_prev_Click(object sender, EventArgs e)
+        {
+            currentImage = flagArray[currentIndex -= 1].imgName;
+            
+            imageBox.Url = new Uri($"{imgPath}{currentImage}");
+            Console.WriteLine(imageBox.Url);
+        }
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+            currentImage = flagArray[currentIndex += 1].imgName;
+
+            imageBox.Url = new Uri($"{imgPath}{currentImage}");
+            Console.WriteLine(imageBox.Url);
+        }
+
+        private void btn_guess_Click(object sender, EventArgs e)
+        {
+            //textBox.Text = imageBox.Url.ToString().Split('.').First().Split('/').Last().Remove(0, 8).Replace('_', ' ');
+        }
+    }
+}
