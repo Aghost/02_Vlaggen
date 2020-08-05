9a4f574fdbadb55e7950d4ee8cc1ea2c16805840
youri <a;sldkfjfjdksla>
2020-08-06 00:14:01 +0200

    added points system, fixed bugs

--- 02_vlaggen/Form1.Designer.cs
+++ 02_vlaggen/Form1.Designer.cs
@@ -34,6 +34,10 @@
             this.textBox = new System.Windows.Forms.TextBox();
             this.btn_guess = new System.Windows.Forms.Button();
             this.resizeSvg = new System.Windows.Forms.Button();
+            this.lbl_Total = new System.Windows.Forms.Label();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.lbl_Status = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // imageBox
@@ -50,22 +54,30 @@
             // 
             // btn_prev
             // 
+            this.btn_prev.BackColor = System.Drawing.Color.Indigo;
+            this.btn_prev.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_prev.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_prev.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btn_prev.Location = new System.Drawing.Point(12, 281);
             this.btn_prev.Name = "btn_prev";
             this.btn_prev.Size = new System.Drawing.Size(75, 67);
             this.btn_prev.TabIndex = 1;
-            this.btn_prev.Text = "btn_prev";
-            this.btn_prev.UseVisualStyleBackColor = true;
+            this.btn_prev.Text = "prev";
+            this.btn_prev.UseVisualStyleBackColor = false;
             this.btn_prev.Click += new System.EventHandler(this.btn_prev_Click);
             // 
             // btn_next
             // 
+            this.btn_next.BackColor = System.Drawing.Color.Indigo;
+            this.btn_next.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_next.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_next.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btn_next.Location = new System.Drawing.Point(444, 281);
             this.btn_next.Name = "btn_next";
             this.btn_next.Size = new System.Drawing.Size(75, 67);
             this.btn_next.TabIndex = 2;
-            this.btn_next.Text = "btn_next";
-            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Text = "next";
+            this.btn_next.UseVisualStyleBackColor = false;
             this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
             // 
             // textBox
@@ -75,33 +87,93 @@
             this.textBox.Name = "textBox";
             this.textBox.Size = new System.Drawing.Size(345, 31);
             this.textBox.TabIndex = 3;
+            this.textBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             // 
             // btn_guess
             // 
+            this.btn_guess.BackColor = System.Drawing.Color.Indigo;
+            this.btn_guess.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_guess.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_guess.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btn_guess.Location = new System.Drawing.Point(93, 318);
             this.btn_guess.Name = "btn_guess";
             this.btn_guess.Size = new System.Drawing.Size(345, 30);
             this.btn_guess.TabIndex = 4;
-            this.btn_guess.Text = "btn_guess";
-            this.btn_guess.UseVisualStyleBackColor = true;
+            this.btn_guess.Text = "guess";
+            this.btn_guess.UseVisualStyleBackColor = false;
             this.btn_guess.Click += new System.EventHandler(this.btn_guess_Click);
             // 
             // resizeSvg
             // 
-            this.resizeSvg.Location = new System.Drawing.Point(546, 222);
+            this.resizeSvg.BackColor = System.Drawing.Color.Indigo;
+            this.resizeSvg.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.resizeSvg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resizeSvg.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.resizeSvg.Location = new System.Drawing.Point(526, 281);
             this.resizeSvg.Name = "resizeSvg";
-            this.resizeSvg.Size = new System.Drawing.Size(114, 61);
+            this.resizeSvg.Size = new System.Drawing.Size(132, 67);
             this.resizeSvg.TabIndex = 5;
             this.resizeSvg.Text = "Resize SVG";
-            this.resizeSvg.UseVisualStyleBackColor = true;
+            this.resizeSvg.UseVisualStyleBackColor = false;
             this.resizeSvg.Click += new System.EventHandler(this.resizeSvg_Click);
+            // 
+            // lbl_Total
+            // 
+            this.lbl_Total.AutoSize = true;
+            this.lbl_Total.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Total.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_Total.Location = new System.Drawing.Point(523, 12);
+            this.lbl_Total.Name = "lbl_Total";
+            this.lbl_Total.Size = new System.Drawing.Size(103, 16);
+            this.lbl_Total.TabIndex = 6;
+            this.lbl_Total.Text = "Total Images:";
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.BackColor = System.Drawing.Color.Indigo;
+            this.btn_Start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Start.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Start.Location = new System.Drawing.Point(12, 12);
+            this.btn_Start.Margin = new System.Windows.Forms.Padding(1);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(507, 263);
+            this.btn_Start.TabIndex = 7;
+            this.btn_Start.Text = "&START";
+            this.btn_Start.UseVisualStyleBackColor = false;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // lbl_Status
+            // 
+            this.lbl_Status.AutoSize = true;
+            this.lbl_Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Status.ForeColor = System.Drawing.Color.Maroon;
+            this.lbl_Status.Location = new System.Drawing.Point(523, 121);
+            this.lbl_Status.Name = "lbl_Status";
+            this.lbl_Status.Size = new System.Drawing.Size(19, 16);
+            this.lbl_Status.TabIndex = 8;
+            this.lbl_Status.Text = "H";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(523, 216);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(53, 16);
+            this.lblScore.TabIndex = 10;
+            this.lblScore.Text = "Score:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.SlateGray;
-            this.ClientSize = new System.Drawing.Size(672, 357);
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(744, 357);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lbl_Status);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.lbl_Total);
             this.Controls.Add(this.resizeSvg);
             this.Controls.Add(this.btn_guess);
             this.Controls.Add(this.textBox);
@@ -110,7 +182,7 @@
             this.Controls.Add(this.imageBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "PictureTrainer";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -125,6 +197,10 @@
         private System.Windows.Forms.TextBox textBox;
         private System.Windows.Forms.Button btn_guess;
         private System.Windows.Forms.Button resizeSvg;
+        private System.Windows.Forms.Label lbl_Total;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Label lbl_Status;
+        private System.Windows.Forms.Label lblScore;
     }
 }
 
--- 02_vlaggen/Form1.cs
+++ 02_vlaggen/Form1.cs
@@ -1,26 +1,27 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Windows.Forms;
 
 namespace _02_vlaggen
 {
-    
     struct imgStruct
     {
         public String imgName;
         public int imgIndex;
-        // public int error-count;
-        // public bool active;
+        public int errorCount;
     }
 
     public partial class Form1 : Form
     {
         static string imgPath = Directory.GetParent(Directory.GetParent(Environment.CurrentDirectory).ToString()).ToString()
-                        + "\\img\\"; // `cd ../img | bash`
-        //string imgPath = Directory.GetParent(Directory.GetParent(Environment.CurrentDirectory).ToString()).ToString()
-        //                    + "\\img\\"; // `cd ../img | bash`
+                        + "\\img\\";    // `cd ../img | bash`
         int imgCount = Directory.GetFiles($"{imgPath}", "*.svg").Length;
+        int totalCorrect = 0;
+        int totalFailed = 0;
+        int points = 0;
         
         string currentImage = "";
         int currentIndex = 0;
@@ -31,15 +32,28 @@ namespace _02_vlaggen
         {
             InitializeComponent();
             Load_Images();
+            btn_next_Click(null, null);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
         }
 
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btn_guess_Click(sender, new EventArgs());
+                e.SuppressKeyPress = true;
+            }
+
+        }
+
         private void Load_Images()
         {
             imgArray = new imgStruct[imgCount];
+            Update_Label();
+
             DirectoryInfo d = new DirectoryInfo(imgPath);
 
             int icnt = 0;
@@ -47,46 +61,104 @@ namespace _02_vlaggen
             {
                 imgArray[icnt].imgName = file.Name;
                 imgArray[icnt].imgIndex = icnt;
-                
+                imgArray[icnt].errorCount = 5;
+
                 icnt += 1;
             }
 
         }
 
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            btn_Start.Visible = false;
+        }
+
+        private void Update_Label()
+        {
+            imgCount = imgArray.Length;
+            lbl_Total.Text = $"Total Images: {imgCount}\n\rCorrect: {totalCorrect}\n\rFailed: {totalFailed}";
+            lblScore.Text = "Score = \t\t\t" + points.ToString() + "\r\nPotential Points = \t\t+" + imgArray[currentIndex].errorCount;
+        }
+
+        private void Array_Remove()
+        {
+            var imagelist = imgArray.ToList();
+            imagelist.RemoveAt(currentIndex);
+            imgArray = imagelist.ToArray();
+        }
+
         private void btn_prev_Click(object sender, EventArgs e)
         {
-            currentIndex -= 1;
-            currentIndex %= imgArray.Length;
+            if (currentIndex != 0) { currentIndex -= 1; }
 
             currentImage = imgArray[currentIndex].imgName;
             imageBox.Url = new Uri($"{imgPath}{currentImage}");
+            Update_Label();
         }
 
         private void btn_next_Click(object sender, EventArgs e)
         {
-            currentIndex += 1;
-            currentIndex %= imgArray.Length - 1;
-
+            if (currentIndex != imgCount - 1) { currentIndex += 1; }
+            else { currentIndex = 0; }
+            
             currentImage = imgArray[currentIndex].imgName;
             imageBox.Url = new Uri($"{imgPath}{currentImage}");
+            Update_Label();
         }
 
         private void btn_guess_Click(object sender, EventArgs e)
         {
             string answer = imageBox.Url.ToString().Split('.').First().Split('/').Last().Remove(0, 8).Replace('_', ' ');
+   
             if (textBox.Text == answer || textBox.Text == answer.ToLower())
             {
+                points += imgArray[currentIndex].errorCount;
+                
                 var imagelist = imgArray.ToList();
                 imagelist.RemoveAt(currentIndex);
                 imgArray = imagelist.ToArray();
 
-                textBox.Text = "Correct";
-                btn_next_Click(sender, e);
-                Console.WriteLine(imgArray.Length);
+                lbl_Status.Text = $"{answer} is correct!";
+                lbl_Status.ForeColor = Color.Green;
+
+                textBox.Text = "";
+                totalCorrect += 1;
+
+                btn_prev_Click(sender, e);
             }
+            else if (imgArray[currentIndex].errorCount == 1)
+            {
+                lbl_Status.Text = $"The answer was:\n\r{answer}";
+
+                totalFailed += 1;
+                Array_Remove();
+                btn_prev_Click(sender, e);
+            }
+            else
+            {
+                StringBuilder charray = new StringBuilder(answer);
+                int length = answer.Length;
+
+                imgArray[currentIndex].errorCount -= 1;
+
+                int x = 0;
+                while (x < length)
+                {
+                    charray[x] = ('_');
+                    x++;
+                }
+
+                Random randi = new Random();
+                int y = randi.Next(0, length);
+                charray[y] = answer[y];
+
+                lbl_Status.Text = $"Incorrect... \r\nHint: {charray} {x}";
+                lbl_Status.ForeColor = Color.Red;
+            }
+            Update_Label();
 
         }
-
+        
         private void resizeSvg_Click(object sender, EventArgs e)
         {
             /*
@@ -96,5 +168,6 @@ namespace _02_vlaggen
             File.WriteAllText(imgPath + file.Name, text);
             */
         }
+
     }
 }
